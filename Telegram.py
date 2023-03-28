e7ba4bf6998cd08f892cac7a5c7410d2cd8a46f5
Pedro Henrique Lima Pereira <100004751+PedroHLP25@users.noreply.github.com>
2023-03-27 21:29:13 -0300

    Grade

--- Telegram.py
+++ Telegram.py
@@ -9,9 +9,9 @@ bot = telebot.TeleBot(CHAVE_API)
 public_key = chavinha
 private_key = chavinha 2
 
-df = pd.read_csv('csv')
+df = pd.read_csv('arquivo_atualizado.csv')
 
-@bot.message_handler(commands=["opcao1"])
+@bot.message_handler(commands=["grade"])
 def opcao1(mensagem):
     texto = """
     /Grade Completa
@@ -24,31 +24,29 @@ def opcao1(mensagem):
 def filtros(mensagem):
     texto = """
     /Professor
-    /Horario
+    /Grade
     """
     bot.send_message(mensagem.chat.id, texto)
 
-
-@bot.message_handler(commands=["Professor"])
-def professor(mensagem):
-    bot.send_message(mensagem.chat.id, 'Digite o Nome do Professor: ')
+@bot.message_handler(commands=["Grade"])
+def Grade(mensagem):
+    bot.send_message(mensagem.chat.id, 'Digite a disciplina: ')
 
     @bot.message_handler(func=lambda mensagem: True, content_types=['text'])
-    def get_professor(mensagem):
-        for index, nome in df.iterrows():
-            if mensagem.text.strip() == nome['professor']:
-                bot.send_message(mensagem.chat.id, nome['cadeira'])
-                bot.send_message(mensagem.chat.id, nome['horario'])
-                bot.send_message(mensagem.chat.id, nome['dias'])
-                bot.send_message(mensagem.chat.id, 'Digite algo para Prosseguir')
-                bot.register_next_step_handler(mensagem, after_professor)
+    def get_grade(mensagem):
+        for index, texto in df.iterrows():
+            if mensagem.text.strip() in texto['cadeira']:
+                 bot.send_message(mensagem.chat.id, texto['professor'])
+                 bot.send_message(mensagem.chat.id, texto['horario'])
+                 bot.send_message(mensagem.chat.id, texto['dias'])
+                 bot.send_message(mensagem.chat.id, 'Digite algo para Prosseguir')
+                 bot.register_next_step_handler(mensagem, after_grade)
 
-    bot.register_next_step_handler(mensagem, get_professor)
+    bot.register_next_step_handler(mensagem, get_grade)
 
-
-def after_professor(mensagem):
+def after_grade(mensagem):
     texto = """
-        /Professor Novamente
+        /Grade Novamente
         /Menu
         """
     bot.send_message(mensagem.chat.id, texto)
@@ -77,14 +75,29 @@ def after_bolsa(mensagem):
         """
     bot.send_message(mensagem.chat.id, texto)
 
+@bot.message_handler(commands=["Professor"])
+def professor(mensagem):
+    bot.send_message(mensagem.chat.id, 'Digite o Nome do Professor: ')
 
-@bot.message_handler(commands=["opcao3"])
-def opcao3(mensagem):
+    @bot.message_handler(func=lambda mensagem: True, content_types=['text'])
+    def get_professor(mensagem):
+        for index, nome in df.iterrows():
+            if mensagem.text.strip() == nome['professor']:
+                bot.send_message(mensagem.chat.id, nome['cadeira'])
+                bot.send_message(mensagem.chat.id, nome['horario'])
+                bot.send_message(mensagem.chat.id, nome['dias'])
+                bot.send_message(mensagem.chat.id, 'Digite algo para Prosseguir')
+                bot.register_next_step_handler(mensagem, after_professor)
+
+    bot.register_next_step_handler(mensagem, get_professor)
+
+
+def after_professor(mensagem):
     texto = """
-    Teste
-    """
+        /Professor Novamente
+        /Menu
+        """
     bot.send_message(mensagem.chat.id, texto)
-
 
 @bot.message_handler(commands=["adm"])
 def adm(mensagem):
@@ -114,8 +127,8 @@ def verificar(mensagem):
 def Menu(mensagem):
     texto = """
     Escolha uma opção para continuar:
-    /opcao1 Consultar Grade
-    /opcao2 Consultar Bolsas
+    /grade
+    /Bolsas
     /adm
     Clique em uma das 3 opções
     """
